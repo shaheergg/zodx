183a1955d3e9ea12ed7dd0fe5dab8896797ad85e
shaheersystems <shaheer.imran1429@gmail.com>
2024-03-01 23:58:31 +0500

    FURTHER DESIGN IMPROVMENTS

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,35 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 function Progress({ title, percentage }) {
-  const [bgColor, setBgColor] = useState("");
-  const [flag, setFlag] = useState(false);
-
-  // after every 5 seconds, the progress bar color will change
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setFlag((prev) => !prev);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-  const randomLightColor = () => {
-    const colors = [
-      "bg-red-500",
-      "bg-yellow-500",
-      "bg-green-500",
-      "bg-blue-500",
-      "bg-indigo-500",
-      "bg-purple-500",
-      "bg-pink-500",
-    ];
-    return colors[Math.floor(Math.random() * colors.length)];
-  };
-
-  useEffect(() => {
-    setBgColor(randomLightColor());
-  }, [flag]);
-
   return (
-    <div className="flex items-center justify-between py-2 cursor-grab">
+    <div className="flex items-center justify-between py-2">
       <div className="w-full space-y-2 ">
         <div className="flex items-center gap-4">
           <span className="w-56 text-sm">{title}</span>
@@ -39,7 +12,7 @@ function Progress({ title, percentage }) {
               style={{
                 width: percentage,
               }}
-              className={`h-full transition-all ${bgColor} duration-500 ease-in-out`}
+              className={`h-full transition-all bg-indigo-600 hover:bg-indigo-500 duration-500 ease-in-out`}
             ></div>
           </div>
           <span className="text-xs font-semibold text-gray-200">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,53 +1,13 @@
 import React, { useState } from "react";
 import Progress from "./Progress";
 import { Reorder } from "framer-motion";
+import { data } from "../constants";
 const Skills = () => {
-  const data = [
-    {
-      id: 1,
-      title: "React JS for Web Development",
-      percentage: "99%",
-    },
-    {
-      id: 2,
-      title: "Node JS for Backend Development",
-      percentage: "60%",
-    },
-    {
-      id: 3,
-      title: "Prisma ORM for Database Management",
-      percentage: "40%",
-    },
-    {
-      id: 4,
-      title: "Python Flask for API Development",
-      percentage: "69%",
-    },
-    {
-      id: 5,
-      title: "C++ for Competitive Programming",
-      percentage: "80%",
-    },
-    {
-      id: 6,
-      title: "NextJS for enterprise-level applications",
-      percentage: "80%",
-    },
-  ];
-
-  const [skills, setSkills] = useState(data);
-
   return (
     <div className="overflow-x-auto">
-      <Reorder.Group axis="y" values={skills} onReorder={setSkills}>
-        {skills.map((skill) => {
-          return (
-            <Reorder.Item key={skill.id} value={skill}>
-              <Progress {...skill} key={skill.id} />
-            </Reorder.Item>
-          );
-        })}
-      </Reorder.Group>
+      {data.skills.map((skill) => {
+        return <Progress {...skill} key={skill.id} />;
+      })}
     </div>
   );
 };
